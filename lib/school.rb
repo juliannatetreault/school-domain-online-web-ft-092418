9f8d603099475d038d727c3cd4682a1c507bf040
Julianna Tetreault <juliannatetreault@icloud.com>
2018-09-30 04:17:51 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
 # code here!
+class School
+  
+  attr_reader :name, :roster
+
+def initialize(name) 
+  @name = name 
+  @roster = {}
+end
+
+def add_student(name, grade)
+ @roster[grade] ||= [] 
+  @roster[grade] << name 
+end
+
+def grade(grade)
+    return @roster[grade]
+end
+
+def sort
+  @roster.each do |grade, name|
+  Hash[@roster.sort]
+  #return @roster.sort #{ |a, b| a <=> b }
+  end
+end
